eb6abfae9934ce233e694c0a07ed267db6b7df39
Philip Rideout <philip@napofearth.com>
2015-07-22 19:17:04 -0700

    Simplify the unit test.

--- test/test_server.py
+++ test/test_server.py
@@ -1,15 +1,15 @@
-from subprocess import Popen, PIPE
 import pytest
 import requests
-import os.path
+import os
 
 TIMEOUT = 5
 
 
 @pytest.fixture(scope='module')
 def renderfarm(request):
+    from subprocess import Popen
     import time
-    child = Popen(['python', 'src/server.py'], stdout=PIPE, bufsize=1)
+    child = Popen(['python', 'src/server.py'])
     time.sleep(0.5)
     request.addfinalizer(lambda: child.terminate())
     return child
