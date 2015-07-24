c7563900b16defc301d20a027c2cf560083d685a
Philip Rideout <philip@napofearth.com>
2015-07-23 19:57:44 -0700

    Minor correction.

--- env.sh
+++ env.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 boot2docker up
-docker build .
 eval "$(boot2docker shellinit)"
+docker build .
 docker run -p 8082:8082 -itd -v $(pwd):/island --name=island island bash
 alias island-bash="docker start island && docker attach island"
 alias island-kill="docker rm -f island"
