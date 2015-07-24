47366b6a02f77e2cbb7af6bce38787db14e620ed
Philip Rideout <philip@napofearth.com>
2015-07-23 20:03:13 -0700

    Fix typo.

--- env.sh
+++ env.sh
@@ -2,7 +2,7 @@
 
 boot2docker up
 eval "$(boot2docker shellinit)"
-docker build .
+docker build -t island .
 docker run -p 8082:8082 -itd -v $(pwd):/island --name=island island bash
 alias island-bash="docker start island && docker attach island"
 alias island-kill="docker rm -f island"
